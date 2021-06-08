c593c4838299ab3bb6b4b97c31d362e00674baf0
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-06-08 22:44:04 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -1,20 +1,5 @@
-const fetch = require("node-fetch");
-
-const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
-
 module.exports = {
-  onPreBuild: async ({ event, context }) => {
-
-  try {
-    const response = await fetch(API_ENDPOINT);
-    const data = await response.json();
-    console.log(JSON.stringify({ data }));
-    //return { statusCode: 200, body: JSON.stringify({ data }) };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed fetching data' }),
-    };
-  }}
-};
+  onPreBuild: ({ constants }) => {
+    console.log(constants)
+  },
+}
