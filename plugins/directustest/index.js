d1021a5ee481f1e5dd61970ee636bfd0587e331c
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-06-08 16:05:40 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -24,17 +24,17 @@ module.exports = {
     }
   });
 
-fs.writeFile("./content/blog/helloworld.md", 'Hello World!', function (err) {
-  if (err) return console.log(err);
-  console.log('Hello World > helloworld.md');
-});
+
       //import modules and define some terms
       try {
       //get sanity data
           fetch('https://cms.bimp.be/items/pages')
   .then(res => res.text())
   .then(text => {console.log(text);
-                console.log('api uitgelezen');
+                fs.writeFile("./content/blog/helloworld.md", 'Hello World!', function (err) {
+  if (err) return console.log(err);
+  console.log('Hello World > helloworld.md');
+});
                 });
           
       //then output as md files in our content folder
