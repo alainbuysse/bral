526524b55f55d82287ca2e1ad2c12b09b18e3b37
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-06-08 15:48:59 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -1,47 +1,45 @@
 module.exports = {
-    onPreBuild: async ({ utils, packageJson }) => {
-    console.log("my plugin loaded!")
-        const fs = require("fs-extra");
-        const fetch = require('node-fetch');
-        
-    //add any serializers for your portable text
-    const serializers = {
-      types: {
-        code: (props) =>
-          "```" + props.node.language + "\n" + props.node.code + "\n```",
-      },
-    };
-        
-    fs.readdir("./content/blog", (err, files) => {
-      if (err) console.log(err);
-      else {
-        files.forEach((file) => {
-          console.log(`Deleting: ${file}`);
-          fs.unlink(`content/blog//${file}`, (err) => {
-            if (err) throw err;
-          });
+  onPreBuild: async ({ utils, packageJson }) => {
+  console.log("my plugin loaded!")
+      const fs = require("fs-extra");
+      const fetch = require('node-fetch');
+      
+  //add any serializers for your portable text
+  const serializers = {
+    types: {
+      code: (props) =>
+        "```" + props.node.language + "\n" + props.node.code + "\n```",
+    },
+  };
+      
+  fs.readdir("./content/blog", (err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach((file) => {
+        console.log(`Deleting: ${file}`);
+        fs.unlink(`content/blog//${file}`, (err) => {
+          if (err) throw err;
         });
-      }
-    });
-        fetch1('https://google.com')
-    .then(res => res.text())
-    .then(text => console.log(text))
+      });
+    }
+  });
 
-    };
-    
-        //import modules and define some terms
-        try {
-            console.log('api nog niet uitgelezen');
-        //get sanity data
-            fetch('https://cms.bimp.be/items/pages')
+  fetch('https://google.com')
     .then(res => res.text())
-    .then(text => {console.log(text);
-                  console.log('api uitgelezen');
-                  });
-            
-        //then output as md files in our content folder
-        } catch (error) {
-            utils.build.failBuild('Failure message', { error })
-          }
+    .then(text => console.log(text));
+  
+      //import modules and define some terms
+      try {
+      //get sanity data
+          fetch('https://cms.bimp.be/items/pages')
+  .then(res => res.text())
+  .then(text => {console.log(text);
+                console.log('api uitgelezen');
+                });
+          
+      //then output as md files in our content folder
+      } catch (error) {
+          utils.build.failBuild('Failure message', { error })
         }
-    ,
+      },
+  }
