bcc4ee7e7dd43ae689eefa57cfb36cf60a09898c
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-06-10 11:47:51 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -34,7 +34,7 @@ module.exports = {
   const response = await fetch('https://cms.bimp.be/items/pages');
   // waits until the request completes...
   console.log(response);
-      fs.writeFile("./content/blog/helloworld.md", '---  \ntitle: dit is de titel  \ndate: 2020-02-22T08:31:25.046Z  \ndescription: kleine omschrijving  \n---!  \n', function (err) {
+      fs.writeFile("./content/blog/helloworld3.md", '---  \ntitle: dit is de titel3  \ndate: 2020-02-22T08:31:25.046Z  \ndescription: kleine omschrijving  \n---!  \n', function (err) {
   if (err) return console.log(err);
   console.log('Hello World > helloworld.md');
 });
