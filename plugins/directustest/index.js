bc036fbbe56162fcb93c3a0f458586cb817f9e63
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-06-11 14:17:59 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -25,7 +25,14 @@ module.exports = {
     }
   });
 
-   
+   return fetch('https://cms.bimp.be/items/pages', { headers: { Accept: "application/json" } })
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
+    
     
      fetch('https://cms.bimp.be/items/pages')
     .then(res => res.text())
