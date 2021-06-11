2921cbb5cd1828bd4e2d77a55ae561a97f350aa0
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-06-11 12:32:22 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -24,11 +24,7 @@ module.exports = {
     }
   });
 
-    
-    fs.writeFile("./content/blog/helloworld2.md", '---  \ntitle: dit is de titel2  \ndate: 2021-06-10T08:31:25.046Z  \ndescription: kleine omschrijving  \n---!  \n', function (err) {
-  if (err) return console.log(err);
-  console.log('Hello World2 > helloworld2.md');
-});
+
     
 
 fetch('https://cms.bimp.be/items/pages')
