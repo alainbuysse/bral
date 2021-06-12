a06ffe0e2dc48339311dc995dd8ccd8a375464d6
alainbuysse <57804638+alainbuysse@users.noreply.github.com>
2021-06-12 11:07:19 +0200

    Update index.js

--- plugins/directustest/index.js
+++ plugins/directustest/index.js
@@ -1,24 +1,22 @@
-
 module.exports = {
-  onPreBuild: async ({ utils, packageJson }) => {
-    
-  console.log("first output!")
-    
-      const fs = require("fs-extra");
-      const fetch = require('node-fetch');
-    
+    onPreBuild: async ({ utils, packageJson }) => {
+        
+    console.log("first output!");
+        
+    const fs = require("fs-extra");
+    const fetch = require('node-fetch');
+      
     fs.writeFile("./content/blog/test.md", "dummy txt", function (err) {
         if (err) return console.log(err);
-        console.log('file written');
-      })
-  
+            console.log('file written');
+        });
+    
     fetch('https://cms.bimp.be/items/pages').then(response =>{
         console.log(response,'resolved');
         return response.json(); 
     }).then((data => {
-        console.log(data)
+            console.log(data)
     })).catch(err =>{
-        console.log(err,'err')
+        console.log('error',err)
     });
-    
-      }}
+}}
